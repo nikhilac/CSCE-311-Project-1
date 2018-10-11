2fd6f54a3ecd02495f986d0dfc54a68b319c1a8d
Jereman <jeremy@jduncan.com>
2018-10-11 16:55:24 -0400

    I'm sorry I'm this bad at git lmao

--- project1/projectheader.h
+++ project1/projectheader.h
@@ -1,6 +1,10 @@
 #ifndef PROJECT_HEADER
 #define PROJECT_HEADER
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 void pipefun(char text[]);
 void socketfun(char text[]);
 void sharedmemfun(char text[]);
--- project1/sharedmemfun.c
+++ project1/sharedmemfun.c
@@ -3,6 +3,25 @@
 
 
 void sharedmemfun(char text[]){
-  printf("%s", text);
+
+  FILE *fp;
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+
+  fp = fopen(text, "r");
+
+  if(fp == NULL){
+    printf("Cannot open file");
+    exit(1);
+  }
+
+
+  while((read = getline(&line, &len, fp)) != -1){
+     printf("%s", line);
+  }
+
+  fclose(fp);
   return;
 }        
