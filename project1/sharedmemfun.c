0dc686c229ccd73ddb87ea51df8c1c335483f116
Jereman <jeremy@jduncan.com>
2018-10-13 14:01:04 -0400

    Implimented fork

--- project1/sharedmemfun.c
+++ project1/sharedmemfun.c
@@ -15,46 +15,20 @@ void sharedmemfun(char text[], char searchstring[]){
   size_t len = 0;
   ssize_t read;
 
-  void* shmem = make_shared_mem(128);
+  void* shmem1 = make_shared_mem(128);
+  void* shmem2 = make_shared_mem(128);
+  void* shmem3 = make_shared_mem(128);
+  void* shmem4 = make_shared_mem(128);
+
   char* testmess = "openthedoor\n";
 
-  /*
-  //make shared mem
-  int shmid;
-  key_t shmkey = 12345;
-  shmid = shmget(shmkey,1024, 0666 | IPC_CREAT);
-  char *shmpointer = shmat(shmid, NULL, 0);
-  */
-  fp = fopen(text, "r");
-  if(fp == NULL){
-      printf("Cannot open file\n");
-      exit(1);
-  }
   
-  //char bigstr[] = "";
-  while((read = getline(&line, &len, fp)) != -1){
-    //printf("real line:\n%s", line);
-    //strcat(line, "\n");
-    //printf(line);
-    //printf("%d", strlen(line));
-    int realLength = strlen(line);
-    memcpy(shmem, line, realLength);
-    printf(shmem);
-    printf("\n");
-  }
-  
-  //strncpy(shmem, bigstr, sizeof(bigstr));
-  //printf("%s", shmem);
-  //strncpy(shmpointer, testmess, sizeof(testmess));
-
-  //printf("%s", shmpointer);
-
   pid_t pid = fork();
 
   //only open the file if we're the parent
   if(pid != 0){ 
  
-    //printf("Parent: opening file and reading lines\n");  
+    printf("Parent: opening file and reading lines\n");  
     fp = fopen(text, "r");
 
     if(fp == NULL){
@@ -63,26 +37,28 @@ void sharedmemfun(char text[], char searchstring[]){
     }
 
     while((read = getline(&line, &len, fp)) != -1){
-     // strncpy(shmpointer, line, sizeof(line));
+      int realLength = strlen(line);
+      memcpy(shmem1, line, realLength);
+      //printf(shmem);
+      //printf("\n");
     }
-   // printf("%s", shmpointer);
-    wait(); 
+    wait();
+
   }
 
   //print lines if we're the child
   if(pid == 0){
-    //printf("Child: printing lines\n");
-    //printf("%s", shmem);
+    printf("Child: printing lines\n");
+    printf(shmem1);
     exit(0);
   }
 
   //close file and print lines if we're the parent
   if (pid != 0){
-    //printf("Parent: close file\n");
+    printf("Parent: close file\n");
     fclose(fp);
   }
-//  shmdt(&shmpointer);
-  //shmctl(shmid,IPC_RMID, NULL);
 
   return;
-}        
+
+}//end of sharedmemfun        
